8c3e55595ee31bba86d0b97e9c819363631c16c0
stetsenko.v.n <stetsenko.v.n@gmail.com>
2021-07-04 19:55:00 +0300

    fixed GenreRepository

--- src/main/java/com/example/library/repositories/GenreRepository.java
+++ src/main/java/com/example/library/repositories/GenreRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-
     List<Genre> getGenreByIdIn(List<Long> ids);
 
-    @Query("UPDATE Genre SET genreName = :name WHERE id = :id")
-    void updateGenreById(long id, String name);
 }
